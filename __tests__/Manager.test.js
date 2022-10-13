2116281f85ed14f5685485497e1e8684c3e8f421
amatos <amatos201@gmail.com>
2022-10-13 01:39:29 -0400

    ass most code

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,7 +1,20 @@
-const Employee = require("./employee");
+const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
 
-class Manager extends Employee {
+test("Can set office number via constructor argument", () => {
+  const testValue = 100;
+  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  expect(e.officeNumber).toBe(testValue);
+});
 
-  constructor(name, id, email, officeNumber) {
-    
-module.exports = Manager;
+test("getRole() should return \"Manager\"", () => {
+  const testValue = "Manager";
+  const e = new Manager("Foo", 1, "test@test.com", 100);
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get office number via getOffice()", () => {
+  const testValue = 100;
+  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  expect(e.getOffice()).toBe(testValue);
+});
